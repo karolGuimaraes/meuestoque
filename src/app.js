5e61e2eba037da8f730b76b4c8049cddab3903dc
Vinicius Menezes <vinicius.menezes@meta.com.br>
2020-10-13 12:00:31 -0300

    use cors

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ const host = process.env.HOST || 'localhost';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 const userRoute = require('./routes/userRoute');
 const productRoute = require('./routes/productRoute');
